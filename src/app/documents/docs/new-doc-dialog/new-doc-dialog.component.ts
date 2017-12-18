91e8d655ed94f5778bfa3464e6282274b287b9e7
HungDQ <hung.do.quang@ntq-solution.com.vn>
2017-12-18 18:06:59 +0700

    Add thumb image for doc

--- src/app/documents/docs/new-doc-dialog/new-doc-dialog.component.ts
+++ src/app/documents/docs/new-doc-dialog/new-doc-dialog.component.ts
@@ -1,37 +1,54 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {MatDialogRef} from '@angular/material';
 import {Router} from '@angular/router';
+import {CARD_LANGUAGE} from '../../doc/writing-practice-dialog/writing-practice-dialog-config.model';
 
 @Component({
   selector: 'app-new-doc-modal',
   templateUrl: './new-doc-dialog.component.html',
   styleUrls: ['./new-doc-dialog.component.css']
 })
-export class NewDocModalComponent implements OnInit {
+export class NewDocModalComponent implements OnInit, OnChanges {
 
-  public langLeft: string;
-  public langRight: string;
+  cardLangEnum: typeof CARD_LANGUAGE = CARD_LANGUAGE;
+
+  public langLeft: CARD_LANGUAGE;
+  public langRight: CARD_LANGUAGE;
 
   constructor(public dialogRef: MatDialogRef<NewDocModalComponent>, private router: Router) {
-    this.langLeft = 'English';
-    this.langRight = 'Vietnamese';
+    this.langLeft = CARD_LANGUAGE.ENGLISH;
+    this.langRight = CARD_LANGUAGE.VIETNAMESE;
   }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 
-  onChangeLang(param: any): void {
-    console.log(param);
-    const tmp = this.langLeft;
-    this.langLeft = this.langRight;
-    this.langRight = tmp;
-    console.log(this.langLeft, this.langRight);
+  onChangeLangLeft(language: CARD_LANGUAGE): void {
+    console.log(language);
+    this.langRight = this.getAlternativeLang(language);
+  }
+
+  onChangeLangRight(language: CARD_LANGUAGE): void {
+    console.log(language);
+    this.langLeft = this.getAlternativeLang(language);
+  }
+
+  getAlternativeLang(language: CARD_LANGUAGE) {
+    if (language === CARD_LANGUAGE.ENGLISH) {
+      return CARD_LANGUAGE.VIETNAMESE;
+    }
+    if (language === CARD_LANGUAGE.VIETNAMESE) {
+      return CARD_LANGUAGE.ENGLISH;
+    }
   }
 
   redirect(): void {
     this.dialogRef.close();
-    this.router.navigate(['./pronunciation']);
+    this.router.navigate(['/docs:16']);
   }
 
 }
